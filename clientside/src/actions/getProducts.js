f6326fff1af3e7909bcc023a1d99d7fca1f7f3eb
JustBecerra <justj.becerra@gmail.com>
2022-03-20 18:12:30 -0300

    problema en la accion

--- clientside/src/actions/getProducts.js
+++ clientside/src/actions/getProducts.js
@@ -2,14 +2,12 @@ import axios from "axios";
 const urlapi = "/products";
 
 export async function getProducts(){
-    let info = await axios(urlapi)
-    console.log("action: ", info)
-    return {type: "GET_PRODUCTS", payload:info.data}
+    try{
+        let info = await axios(urlapi)
+        console.log("action: ", info)
+        return {type: "GET_PRODUCTS", payload:info.data}
+    }catch(err){
+        console.log(err.message)
+    }
+    
 }
-
-// export function getDiets(){
-//     return async function(dispatch){
-//         let json = await(await axios.get(/types)).data;
-//         return dispatch({type: GET_DIETS, payload: json})
-//     }
-// }
